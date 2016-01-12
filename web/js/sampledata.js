c5b694581bd3cef7ac23d7b711b4078b8c654cd7
semeon <semeon.peradze@gmail.com>
2016-01-12 00:31:36 -0800

    Can draw with Dagre

---
+++ web/js/sampledata.js
@@ -0,0 +1,42 @@
+var sampleData = {  "title":"Root",
+					"children":[
+						{"title":"Node A1", "children":[
+							{"title":"Node A11", "children":null},
+							{"title":"Node A12", "children":null},
+							{"title":"Node A13", "children":null},
+							{"title":"Node A14", "children":null}
+						]},
+
+						{"title":"Node A2", "children":[
+							{"title":"Node A21", "children":null},
+							{"title":"Node A22", "children":[
+								{"title":"Node A221", "children":null},
+								{"title":"Node A222", "children":null},
+								{"title":"Node A223", "children":null},
+								{"title":"Node A224", "children":null}
+							]},
+							{"title":"Node A23", "children":null},
+							{"title":"Node A24", "children":null}
+						]},
+
+						{"title":"Node A3", "children":[
+							{"title":"Node A31", "children":null},
+							{"title":"Node A32", "children":null},
+							{"title":"Node A33", "children":null},
+							{"title":"Node A34", "children":null}
+						]},
+
+						{"title":"Node A4", "children":[
+							{"title":"Node A41", "children":null},
+							{"title":"Node A42", "children":null},
+							{"title":"Node A43", "children":null},
+							{"title":"Node A44", "children":null}
+						]},
+
+						{"title":"Node A5", "children":null}
+					]
+				};
+
+
+
+
