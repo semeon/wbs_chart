00c116224b23ed3d9859a6c46efb1e261e172476
semeon <semeon.peradze@gmail.com>
2016-01-14 00:26:15 -0800

    cleanup unused code

--- web/js/graph/graphController.js
+++ web/js/graph/graphController.js
@@ -1,22 +1,6 @@
 import * as dataParser from "js/graph/dataProcessor.js";
 import * as dagre from "js/graph/dGraph.js";
 
-
-export var controller = new GraphController();
-
-function GraphController() {
-
-	this.draw = function(text) {
-		var textLines = dataParser.textToLines(text);
-		var graphData = dataParser.linesToGraphObjects(textLines);
-
-		if (graphData.length>0) {
-			var graphController = new dagre.graphController("g1", "svg#g1");
-			graphController.draw(graphData);
-		}
-	}
-}
-
 export function draw(text) {
 	var textLines = dataParser.textToLines(text);
 	var graphData = dataParser.linesToGraphObjects(textLines);
