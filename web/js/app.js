1df45bfcbc66d3bcee323e49142b3440b99af519
semeon <semeon.peradze@gmail.com>
2016-02-01 18:44:38 -0800

    File structure changes

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,7 @@
 import * as textData from "data/defaultData.js";
 import {Page} from "js/page/pageController.js";
 import {Drawing} from "js/drawing/drawingController.js";
-import {model} from "js/drawing/model/dataModel.js";
+import {model} from "js/model/dataModel.js";
 
 var drawing = new Drawing(model);
 var page = new Page(drawing);
